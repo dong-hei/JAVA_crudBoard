d45b697f9ece989bc20acf86295ee84b82e0466a
DK <101485484+dong-hei@users.noreply.github.com>
2023-05-09 08:58:54 +0900

    Update BoardController.java

--- src/main/java/board_dk/board/controller/BoardController.java
+++ src/main/java/board_dk/board/controller/BoardController.java
@@ -35,20 +35,14 @@ public class BoardController {
 
     @GetMapping("/list")
     public String list(Model model, @PageableDefault(size = 3) Pageable pageable,@RequestParam(required = false, defaultValue = "") String searchText){
-        //@PageableDefault 페이지네이션 사이즈를 안줬을때 디폴트 값을 지정해준다
-//        Page<Board> boards = boardRepository.findAll(PageRequest.of(0,20));
-        //Page로 리턴 받고 PageRequest.of(1,20) 는 PageNation
-//        Page<Board> boards = boardRepository.findAll(pageable);
-        //PageNation을 동적으로 적용될수있게끔 도와주는 코드
+
         Page<Board> boards = boardRepository.findByTitleContainingOrContentContaining(searchText, searchText, pageable);
         int startPage = Math.max(1,boards.getPageable().getPageNumber() -4);
         int endPage = Math.max(boards.getTotalPages() ,boards.getPageable().getPageNumber() +4);
         model.addAttribute("startPage",startPage);
         model.addAttribute("endPage", endPage);
-        //PageNation start 맨 앞페이지 end 맨 뒤페이지 지정
 
         model.addAttribute("boards",boards);
-        //jpa
         return "board/list";
     }
 
@@ -69,10 +63,8 @@ public class BoardController {
       if(bindingResult.hasErrors()){
           return "board/form";
       }
-//       밑의 내용과 같다
-//        Authentication a = SecurityContextHolder.getContext().getAuthentication();
+
         String username = authentication.getName();
-//        사용자 user id를 담아서 게시글 게시시 인증된 회원 아이디로 저장
         boardService.save(username, board);
 
         boardRepository.save(board);
