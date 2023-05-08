32ef8513855f561a1a525dec4a8cf96773427c22
DK <101485484+dong-hei@users.noreply.github.com>
2023-05-09 08:50:08 +0900

    Update WebSecurityConfig.java

--- src/main/java/board_dk/board/config/WebSecurityConfig.java
+++ src/main/java/board_dk/board/config/WebSecurityConfig.java
@@ -22,10 +22,9 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable() //보안체크 하지않는다.실제로 이러면 보안에 위험하다.
+                .csrf().disable() 
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/","account/register","/css/**","/api/**").permitAll()
-                        //이 경로는 승인이 필요없다
                         .anyRequest().authenticated()
                         //
                 )
@@ -34,8 +33,6 @@ public class WebSecurityConfig {
                         .permitAll()
                 )
                 .logout((logout) -> logout.permitAll());
-//어떤 요청이던 인증된
-// 폼 부터는 로그인 성공해야된다.
         return http.build();
     }
     @Autowired
@@ -48,29 +45,14 @@ public class WebSecurityConfig {
                         "select username,password,enabled "
                         + "from user "
                         + "where username = ? ")
-//                끝에 띄어쓰기 해줘야 where절과 붙지않는다.
                 .authoritiesByUsernameQuery("select u.username, r.name "
                         + "from user_role ur inner join user u on ur.user_id = u.id "
                         + "inner join role r on ur.role_id = r.id "
                         + "where u.username = ? ");
-    } // Authetication 로그인 처리
-    //authoritie 권한 처리
+    } 
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    } //안전하게 암호화 할수있는 스프링 코드
+    } 
 }
-
-//임시 유저만들기
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        UserDetails user =
-//                User.withDefaultPasswordEncoder()
-//                        .username("user")
-//                        .password("password")
-//                        .roles("USER")
-//                        .build();
-//        return new InMemoryUserDetailsManager(user);
-//    }
-//}
